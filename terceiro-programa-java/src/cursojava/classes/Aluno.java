09937714f4254e84e453aa0914ffe90b4ca1b26c
Leandro Marques <Leandro Marques@PCGAMER>
2023-12-03 21:37:08 -0300

    Criação da classe Aluno e TerceiroProgramaJava.

---
+++ terceiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,9 @@
+package cursojava.classes;
+
+public class Aluno {
+
+	public Aluno() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ terceiro-programa-java/src/cursojava/executaveis/TerceiroProgramaJava.java
@@ -0,0 +1,10 @@
+package cursojava.executaveis;
+
+public class TerceiroProgramaJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
